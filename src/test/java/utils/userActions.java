5625eab44b3752ed76bc5fe8c3444908fb17cda0
Dinh246 <dinh.truong24695@gmail.com>
2022-08-03 16:19:27 +0700

    Update methods zoom & pinch

--- src/test/java/utils/userActions.java
+++ src/test/java/utils/userActions.java
@@ -32,23 +32,23 @@ public class userActions {
         return this;
     }
 
-    public userActions waitScreenStable(WebElement Element){
+    public userActions waitScreenStable(WebElement Element) {
         WebDriverWait wait = new WebDriverWait(androidDriver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOf(Element));
         return this;
     }
 
-    public userActions swipeLeft(WebElement element){
-        int centerY = element.getRect().y + (element.getSize().height/2);
-        double startX = androidDriver.manage().window().getSize().width*0.75;
-        double endX = androidDriver.manage().window().getSize().width*0.25;
+    public userActions swipeLeft(WebElement element) {
+        int centerY = element.getRect().y + (element.getSize().height / 2);
+        double startX = androidDriver.manage().window().getSize().width * 0.75;
+        double endX = androidDriver.manage().window().getSize().width * 0.25;
 
         finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        swipe = new Sequence(finger,1);
-        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), (int)startX, centerY));
+        swipe = new Sequence(finger, 1);
+        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), (int) startX, centerY));
         swipe.addAction(finger.createPointerDown(0));
-        swipe.addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), (int)endX, centerY));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), (int) endX, centerY));
         swipe.addAction(finger.createPointerUp(0));
 
         androidDriver.perform(List.of(swipe));
@@ -56,31 +56,31 @@ public class userActions {
         return this;
     }
 
-    public userActions swipeRight(WebElement element){
-        int centerY = element.getRect().y + (element.getSize().height/2);
-        double startX = androidDriver.manage().window().getSize().width*0.25;
-        double endX = androidDriver.manage().window().getSize().width*0.9;
+    public userActions swipeRight(WebElement element) {
+        int centerY = element.getRect().y + (element.getSize().height / 2);
+        double startX = androidDriver.manage().window().getSize().width * 0.25;
+        double endX = androidDriver.manage().window().getSize().width * 0.9;
 
         finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        swipe = new Sequence(finger,1);
-        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), (int)startX, centerY));
+        swipe = new Sequence(finger, 1);
+        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), (int) startX, centerY));
         swipe.addAction(finger.createPointerDown(0));
-        swipe.addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), (int)endX, centerY));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), (int) endX, centerY));
         swipe.addAction(finger.createPointerUp(0));
 
         androidDriver.perform(List.of(swipe));
         return this;
     }
 
-    public userActions scrollUp(WebElement element){
-        int centerX = androidDriver.manage().window().getSize().width/2;
-        double startY = element.getRect().y + (element.getSize().height*0.2);
-        double endY = element.getRect().y + (element.getSize().height*0.9);
+    public userActions scrollUp(WebElement element) {
+        int centerX = androidDriver.manage().window().getSize().width / 2;
+        double startY = element.getRect().y + (element.getSize().height * 0.2);
+        double endY = element.getRect().y + (element.getSize().height * 0.9);
 
         finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        scroll = new Sequence(finger,1);
+        scroll = new Sequence(finger, 1);
         scroll.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), centerX, (int) startY));
         scroll.addAction(finger.createPointerDown(0));
         scroll.addAction(finger.createPointerMove(Duration.ofMillis(480), PointerInput.Origin.viewport(), centerX, (int) endY));
@@ -90,14 +90,14 @@ public class userActions {
         return this;
     }
 
-    public userActions scrollDown(WebElement element){
-        int centerX = androidDriver.manage().window().getSize().width/2;
-        double startY = androidDriver.manage().window().getSize().height*0.8;
-        double endY = androidDriver.manage().window().getSize().height*0.2;
+    public userActions scrollDown(WebElement element) {
+        int centerX = androidDriver.manage().window().getSize().width / 2;
+        double startY = androidDriver.manage().window().getSize().height * 0.8;
+        double endY = androidDriver.manage().window().getSize().height * 0.2;
 
         finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        scroll = new Sequence(finger,1);
+        scroll = new Sequence(finger, 1);
         scroll.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), centerX, (int) startY));
         scroll.addAction(finger.createPointerDown(0));
         scroll.addAction(finger.createPointerMove(Duration.ofMillis(480), PointerInput.Origin.viewport(), centerX, (int) endY));
@@ -107,81 +107,158 @@ public class userActions {
         return this;
     }
 
-    public userActions swipeLeftMultipleTimes(WebElement element, Integer times){
-        int centerY = element.getRect().y + (element.getSize().height/2);
-        double startX = androidDriver.manage().window().getSize().width*0.75;
-        double endX = androidDriver.manage().window().getSize().width*0.25;
+    public userActions swipeLeftMultipleTimes(WebElement element, Integer times) {
+        int centerY = element.getRect().y + (element.getSize().height / 2);
+        double startX = androidDriver.manage().window().getSize().width * 0.75;
+        double endX = androidDriver.manage().window().getSize().width * 0.25;
 
         finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        swipe = new Sequence(finger,1);
-        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), (int)startX, centerY));
+        swipe = new Sequence(finger, 1);
+        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), (int) startX, centerY));
         swipe.addAction(finger.createPointerDown(0));
-        swipe.addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), (int)endX, centerY));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), (int) endX, centerY));
         swipe.addAction(finger.createPointerUp(0));
 
-        for (int i = 0; i < times; i++){
+        for (int i = 0; i < times; i++) {
             androidDriver.perform(List.of(swipe));
         }
 
         return this;
     }
 
-    public userActions swipeRightMultipletimes(WebElement element, Integer times){
-        int centerY = element.getRect().y + (element.getSize().height/2);
-        double startX = androidDriver.manage().window().getSize().width*0.25;
-        double endX = androidDriver.manage().window().getSize().width*0.9;
+    public userActions swipeRightMultipletimes(WebElement element, Integer times) {
+        int centerY = element.getRect().y + (element.getSize().height / 2);
+        double startX = androidDriver.manage().window().getSize().width * 0.25;
+        double endX = androidDriver.manage().window().getSize().width * 0.9;
 
         finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        swipe = new Sequence(finger,1);
-        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), (int)startX, centerY));
+        swipe = new Sequence(finger, 1);
+        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0),
+                PointerInput.Origin.viewport(), (int) startX, centerY));
         swipe.addAction(finger.createPointerDown(0));
-        swipe.addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), (int)endX, centerY));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(200),
+                PointerInput.Origin.viewport(), (int) endX, centerY));
         swipe.addAction(finger.createPointerUp(0));
 
-        for (int i = 0; i < times; i++){
+        for (int i = 0; i < times; i++) {
             androidDriver.perform(List.of(swipe));
         }
         return this;
     }
 
-    public userActions scrollUpMultipleTimes(WebElement element, Integer times){
-        int centerX = androidDriver.manage().window().getSize().width/2;
-        double startY = element.getRect().y + (element.getSize().height*0.2);
-        double endY = element.getRect().y + (element.getSize().height*0.9);
+    public userActions scrollUpMultipleTimes(WebElement element, Integer times) {
+        int centerX = androidDriver.manage().window().getSize().width / 2;
+        double startY = element.getRect().y + (element.getSize().height * 0.2);
+        double endY = element.getRect().y + (element.getSize().height * 0.9);
 
         finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        scroll = new Sequence(finger,1);
-        scroll.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), centerX, (int) startY));
+        scroll = new Sequence(finger, 1);
+        scroll.addAction(finger.createPointerMove(Duration.ofSeconds(0),
+                PointerInput.Origin.viewport(), centerX, (int) startY));
         scroll.addAction(finger.createPointerDown(0));
-        scroll.addAction(finger.createPointerMove(Duration.ofMillis(480), PointerInput.Origin.viewport(), centerX, (int) endY));
+        scroll.addAction(finger.createPointerMove(Duration.ofMillis(480),
+                PointerInput.Origin.viewport(), centerX, (int) endY));
         scroll.addAction(finger.createPointerUp(0));
 
-        for (int i = 0; i < times; i++){
+        for (int i = 0; i < times; i++) {
             androidDriver.perform(List.of(scroll));
         }
         return this;
     }
 
-    public userActions scrollDownMultipleTimes(WebElement element, Integer times){
-        int centerX = androidDriver.manage().window().getSize().width/2;
-        double startY = element.getRect().y + (element.getSize().height*0.8);
-        double endY = element.getRect().y + (element.getSize().height*0.2);
+    public userActions scrollDownMultipleTimes(WebElement element, Integer times) {
+        int centerX = androidDriver.manage().window().getSize().width / 2;
+        double startY = element.getRect().y + (element.getSize().height * 0.8);
+        double endY = element.getRect().y + (element.getSize().height * 0.2);
 
         finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-        scroll = new Sequence(finger,1);
-        scroll.addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), centerX, (int) startY));
+        scroll = new Sequence(finger, 1);
+        scroll.addAction(finger.createPointerMove(Duration.ofSeconds(0),
+                PointerInput.Origin.viewport(), centerX, (int) startY));
         scroll.addAction(finger.createPointerDown(0));
-        scroll.addAction(finger.createPointerMove(Duration.ofMillis(480), PointerInput.Origin.viewport(), centerX, (int) endY));
+        scroll.addAction(finger.createPointerMove(Duration.ofMillis(480),
+                PointerInput.Origin.viewport(), centerX, (int) endY));
         scroll.addAction(finger.createPointerUp(0));
 
-        for (int i = 0; i < times; i++){
+        for (int i = 0; i < times; i++) {
             androidDriver.perform(List.of(scroll));
         }
+        return this;
+    }
 
+    public userActions zoom(Integer times) {
+
+        int finger1XStart = androidDriver.manage().window().getSize().width / 2 - 10;
+        int finger1YStart = androidDriver.manage().window().getSize().height / 2 - 10;
+        int finger2XStart = androidDriver.manage().window().getSize().width / 2 + 10;
+        int finger2YStart = androidDriver.manage().window().getSize().height / 2 + 10;
+
+        double finger1XEnd = androidDriver.manage().window().getSize().width * 0.9;
+        double finger1YEnd = androidDriver.manage().window().getSize().height * 0.9;
+        double finger2XEnd = androidDriver.manage().window().getSize().width * 0.1;
+        double finger2YEnd = androidDriver.manage().window().getSize().height * 0.1;
+
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        Sequence swipeUp = new Sequence(finger1, 1);
+        swipeUp.addAction(finger1.createPointerMove(Duration.ofSeconds(0),
+                PointerInput.Origin.viewport(), finger1XStart, finger1YStart));
+        swipeUp.addAction(finger1.createPointerDown(0));
+        swipeUp.addAction(finger1.createPointerMove(Duration.ofMillis(300),
+                PointerInput.Origin.viewport(), (int) finger1XEnd, (int) finger1YEnd));
+        swipeUp.addAction(finger1.createPointerUp(0));
+
+
+        PointerInput finger2 = new PointerInput(PointerInput.Kind.TOUCH, "finger2");
+        Sequence swipeDown = new Sequence(finger2, 1);
+        swipeDown.addAction(finger2.createPointerMove(Duration.ofSeconds(0),
+                PointerInput.Origin.viewport(), finger2XStart, finger2YStart));
+        swipeDown.addAction(finger2.createPointerDown(0));
+        swipeDown.addAction(finger2.createPointerMove(Duration.ofMillis(300),
+                PointerInput.Origin.viewport(), (int) finger2XEnd, (int) finger2YEnd));
+        swipeDown.addAction(finger2.createPointerUp(0));
+
+        for (int i = 0; i < times; i++) {
+            androidDriver.perform(List.of(swipeUp, swipeDown));
+        }
+        return this;
+    }
+
+    public userActions pinch(Integer times) {
+
+        double finger1XStart = androidDriver.manage().window().getSize().width*0.9 ;
+        double finger1YStart = androidDriver.manage().window().getSize().height*0.9;
+        double finger2XStart = androidDriver.manage().window().getSize().width*0.1;
+        double finger2YStart = androidDriver.manage().window().getSize().height*0.1;
+
+        int xEnd = androidDriver.manage().window().getSize().width/2;
+        int yEnd = androidDriver.manage().window().getSize().height/2;
+
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        Sequence swipeUp = new Sequence(finger1, 1);
+        swipeUp.addAction(finger1.createPointerMove(Duration.ofSeconds(0),
+                PointerInput.Origin.viewport(), (int) finger1XStart, (int) finger1YStart));
+        swipeUp.addAction(finger1.createPointerDown(0));
+        swipeUp.addAction(finger1.createPointerMove(Duration.ofMillis(300),
+                PointerInput.Origin.viewport(), xEnd,  yEnd));
+        swipeUp.addAction(finger1.createPointerUp(0));
+
+
+        PointerInput finger2 = new PointerInput(PointerInput.Kind.TOUCH, "finger2");
+        Sequence swipeDown = new Sequence(finger2, 1);
+        swipeDown.addAction(finger2.createPointerMove(Duration.ofSeconds(0),
+                PointerInput.Origin.viewport(), (int) finger2XStart, (int) finger2YStart));
+        swipeDown.addAction(finger2.createPointerDown(0));
+        swipeDown.addAction(finger2.createPointerMove(Duration.ofMillis(300),
+                PointerInput.Origin.viewport(), xEnd, yEnd));
+        swipeDown.addAction(finger2.createPointerUp(0));
+
+        for (int i = 0; i < times; i++) {
+            androidDriver.perform(List.of(swipeUp, swipeDown));
+        }
         return this;
     }
 }
